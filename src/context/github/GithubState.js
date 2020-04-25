cab867466cd7ef1a99d9d68af96f8b66cffd0c20
dbeccaria826 <beccdaniel@gmail.com>
2020-04-25 18:49:23 -0400

    added boilerplate createContext code, separating state from components, passing it around in the GithubContext.Provider

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,51 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_REPOS, GET_USER } from '../types';
+
+
+const GithubState = props => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
+// Actions
+    // Search
+    const searchUsers = async text => {
+        setLoading();
+        const res = await axios.get(
+            `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+            &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        dispatch({
+            type: SEARCH_USERS,
+            payload: res.data
+        });
+         
+     };
+
+    // Get User
+
+    // Get Repos
+
+    // Clear Users
+
+    // Set Loading
+    const setLoading = () => dispatch({type: SET_LOADING})
+    return <GithubContext.Provider value={
+        {
+            users: state.users,
+            user: state.user,
+            repos: state.repos,
+            loading: state.loading
+        }}>
+        
+    </GithubContext.Provider>
+}
+
+export default GithubState;
