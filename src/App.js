a40c19b15edee6623fb6856c457c5e51e2727779
dbeccaria826 <beccdaniel@gmail.com>
2020-04-25 23:13:35 -0400

    removed Search and Users component from App.js and placed them in their own component, Home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
-import Users from './components/Users/Users';
-import Search from './components/Users/Search'
 import User from './components/Users/User';
 import Alert from './components/Layout/Alert';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
 import './App.css';
 
 // React Context API
@@ -22,15 +21,7 @@ const App = () => {
         <div className="container">
             <Alert />
             <Switch>
-              <Route path='/' exact render={props => (
-                <Fragment>
-                  <Search
-                   
-                    // setAlert={setAlert}
-                  />
-                  <Users />
-                </Fragment>
-              )} />
+                <Route path='/' exact component={Home} />
               <Route exact path="/about" component={About} />
               <Route exact path="/user/:login" component={User}/>
             </Switch>
